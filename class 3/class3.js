0af06ec27abe5f2adfc9186d66197ca224b1a41f
valentincruz2005 <93301606+valentincruz2005@users.noreply.github.com>
2021-11-15 20:47:54 -0300

    Add files via upload

--- class 3/class3.js
+++ class 3/class3.js
@@ -1,75 +1,12 @@
-// let suma= 0;
-// let mayoresa5=0;
-// for (let i= 1; i<=10; i++) {
-
-
-// suma=suma+i;
-// if (i=>5) { 
-//     mayoresa5 ++;
-// }
-
-// }
-// alert ('la suma total es' + suma + "la suma total de los numneros mayores a 5 es" , mayoresa5)
-
-
-
-
-// let nombre = prompt ("nombre")
-// let nota = parseFloat ( prompt ("INGRESE LA NOTA"))
-// let sumanotas= 0;
-// let cantidadenotas= 0;
-
-// while (nombre!== "ESC" && nota >=1 && nota <=10) {
-
-// sumanotas =+nota
-// cantidadenotas++
-// alert ("Alumno: " + nombre + "notas obtenidas " + nota)
-
-// let nombre = prompt ("nombre")
-// let nota = parseFloat ( prompt ("INGRESE LA NOTA"))
-
-// }
-// let promedio= sumanotas/cantidadenotas
-// alert ("PROMEDIO " , promedio)
-
-// let clima= prompt ("ingrese como esta el clima")
-// switch (clima.toLowerCase ()) {
-//     case "fresco":
-//     alert ("sali con campera")
-//     break;
-//     case "lluvioso":
-//         alert ("sali con paragua")
-//         break;
-//     case "soleado":
-//         alert ("usa protector solar")
-//         break;
-
-//         default : 
-//         alert ("el tiempo es agradable")
-//         break;
-//}
-
-
-
-
-// let numero = prompt ("INGRESE UN NUMERO")
-// let algo = 0;
-// for ( i=0; i=10;i++) {
-// suma = algo+i;
-
-// let numero = prompt ("INGRESE UN NUMERO")
-// }
-// console.log ("su numero"+ suma )
-// 
 
 //CONSIGNA N1
-// let number = (parseInt (prompt ("ESCRIBA EL RESULTADO DE 151X10")))
-// if (number ==1510) {
-//     alert("FELICIDADES, USTED RESOLVIO EL CALCULO") 
-// }
-// else {
-//     alert ("ESE NO ES EL RESULTADO. SIGA INTENTADOLO")
-// }
+ let number = (parseInt (prompt ("ESCRIBA EL RESULTADO DE 151X10")))
+ if (number ==1510) {
+     alert("FELICIDADES, USTED RESOLVIO EL CALCULO") 
+ }
+ else {
+     alert ("ESE NO ES EL RESULTADO. SIGA INTENTADOLO")
+ }
 
 
 
@@ -78,16 +15,16 @@
 
 
 //CONSIGNA N1
-/*for (i=1;i<=3;i++) {
+for (i=1;i<=3;i++) {
  let number = (parseInt (prompt ("ESCRIBA UN NUMERO")))
  let number2 = (parseInt(prompt ("ESTE NUMERO SE SUMARA CON EL ANTERIOR")))
  let numero= number + number2
   console.log ("SU NUMERO INGRESADO MAS " +number2 + "ES IGUAL A=" +" "+ numero)
-}*/
+}
 
 
 //CONSIGNA N2
-/*let nombre = (prompt ("INGRESE ALGUN NOMBRE"))
+let nombre = (prompt ("INGRESE ALGUN NOMBRE"))
 let apellido = prompt ("INGRESE ALGUN APELLIDO")
 let nombrecompleto= nombre + " "+ apellido
 
@@ -97,18 +34,18 @@ while (( nombre!= "ESC") || (apellido!="ESC")) {
     nombre = prompt ("INGRESE ALGUN NOMBRE - ESC PARA SALIR")
     apellido = prompt ("INGRESE ALGUN APELLIDO - ESC PARA SALIR" )
     nombrecompleto= nombre + " "+ apellido  
-}*/
+}
 
 
 
 //CONSIGNA 3
 
-/*let texto= prompt ("INGRESE UN TEXTO")
+let texto= prompt ("INGRESE UN TEXTO")
 let repeticiones= parseInt (prompt ("INGRESE LA CANTIDAD DE VECES QUE DESEA REPETIRLO"))
 for (i=0;i<repeticiones; i++) {
   console.log ("el texto ingresado es:",texto,i,"se repite: ",repeticiones, )
 
-}*/
+}
 
 
 
